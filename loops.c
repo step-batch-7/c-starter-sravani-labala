75f114a07e66722920bc27333386b5e91e1ea0c5
Sravani Labala <58027100+sravani-labala@users.noreply.github.com>
2020-03-28 22:16:29 +0530

    Added fibonacci series function

--- loops.c
+++ loops.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int factorial(int num);
+int fibonacci(int upto);
 
 // calculating upto 12 numbers
 int factorial(int num)
@@ -8,11 +9,29 @@ int factorial(int num)
   return (num == 0) ? 1 : (num * factorial(num - 1));
 }
 
+int fibonacci(int upto)
+{
+  int previousTerm = 0, currentTerm = 1, temp;
+  while (upto)
+  {
+    printf("%d ", previousTerm);
+    temp = previousTerm;
+    previousTerm = currentTerm;
+    currentTerm = temp + previousTerm;
+    upto--;
+  }
+  return 0;
+}
+
 int main(void)
 {
   int num;
+
   printf("Enter a number\n");
   scanf("%d", &num);
   printf("factorial of %d is %d\n", num, factorial(num));
+  printf("The fibonacci series upto %d are as follows\n", num);
+  fibonacci(num);
+
   return 0;
 }
