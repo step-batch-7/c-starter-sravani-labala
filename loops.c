a46f16fe3821303ca8be342624f843994b72caeb
Sravani Labala <58027100+sravani-labala@users.noreply.github.com>
2020-03-28 22:31:21 +0530

    added sum of any n numbers

--- loops.c
+++ loops.c
@@ -4,6 +4,7 @@ int factorial(int num);
 int fibonacci(int upto);
 int odd_number_series(int upto);
 int even_number_series(int upto);
+int sum_of_n_numbers(int start, int end);
 
 // calculating upto 12 numbers
 int factorial(int num)
@@ -49,19 +50,40 @@ int even_number_series(int upto)
   return 0;
 }
 
+int sum_of_n_numbers(int start, int end)
+{
+  int sum = 0;
+  for (int i = start; i <= end; i++)
+  {
+    sum += i;
+  }
+  return sum;
+}
+
 int main(void)
 {
-  int num1;
+  int num1, num2;
 
   printf("Enter a number\n");
   scanf("%d", &num1);
+
   printf("factorial of %d is %d\n", num1, factorial(num1));
+
   printf("The fibonacci series upto %d are as follows\n", num1);
   fibonacci(num1);
-  printf("\nThe odd number series upto %d is as follows\n", num1);
+  printf("\n");
+
+  printf("The odd number series upto %d is as follows\n", num1);
   odd_number_series(num1);
-  printf("\nThe even number series upto %d is as follows\n", num1);
+  printf("\n");
+
+  printf("The even number series upto %d is as follows\n", num1);
   even_number_series(num1);
+  printf("\n");
+
+  printf("Enter two limits to calculate the sum (ascending order)\n");
+  scanf("%d %d", &num1, &num2);
+  printf("The sum between the limits %d and %d is %d\n", num1, num2, sum_of_n_numbers(num1, num2));
 
   return 0;
 }
