4f78f9ca2ab66cff2e5bf47e60a8ccdb93bba3ab
Sravani Labala <58027100+sravani-labala@users.noreply.github.com>
2020-03-28 16:59:56 +0530

    Modified runC.sh script to create bin folder
    Added convert_fahrenheit_to_centigrade and
    convert_centigrade_to_fahrenheit functions

--- basic_function.c
+++ basic_function.c
@@ -2,8 +2,10 @@
 
 int is_even(int num);
 int is_odd(int num);
-int square(int);
-int cube(int);
+int square(int num);
+int cube(int num);
+float convert_fahrenheit_to_centigrade(float fahrenheit);
+float convert_centigrade_to_fahrenheit(float centigrade);
 
 int is_even(int num)
 {
@@ -26,14 +28,29 @@ int cube(int num)
   return square(num) * num;
 }
 
+float convert_fahrenheit_to_centigrade(float fahrenheit)
+{
+  return (fahrenheit - 32) * 5 / 9;
+}
+
+float convert_centigrade_to_fahrenheit(float centigrade)
+{
+  return ((centigrade * 9) / 5) + 32;
+}
+
 int main(void)
 {
   int num;
+  float temperature;
   printf("Enter a number\n");
   scanf("%d", &num);
   printf("%d is %s\n", num, is_even(num) ? "even" : "not even");
   printf("%d is %s\n", num, is_odd(num) ? "odd" : "not odd");
   printf("square of %d is %d\n", num, square(num));
   printf("cube of %d is %d\n", num, cube(num));
+  printf("Enter the temperature\n");
+  scanf("%f", &temperature);
+  printf("%f fahrenheit converted to centigrade is %f\n", temperature, convert_fahrenheit_to_centigrade(temperature));
+  printf("%f centigrade converted to fahrenheit is %f\n", temperature, convert_centigrade_to_fahrenheit(temperature));
   return 0;
 }
--- runC.sh
+++ runC.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+mkdir -p bin
 fileName=$1
 compileFile=`echo $fileName | sed "s/.c$//"`
 gcc -o bin/$compileFile ./$fileName && bin/$compileFile
+rm -rf bin
