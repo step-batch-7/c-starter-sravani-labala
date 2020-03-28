54381b4176b58ee3dcf9d294b3e9a62edcde907c
Sravani Labala <58027100+sravani-labala@users.noreply.github.com>
2020-03-28 21:17:04 +0530

    Added simple_interest function

--- basic_function.c
+++ basic_function.c
@@ -2,12 +2,13 @@
 
 int is_even(int num);
 int is_odd(int num);
-int square(int num);
-int cube(int num);
+long int square(int num);
+long int cube(int num);
 float convert_fahrenheit_to_centigrade(float fahrenheit);
 float convert_centigrade_to_fahrenheit(float centigrade);
 int greatest_of_three(int num1, int num2, int num3);
 float average_of_three(int num1, int num2, int num3);
+float simple_interest(long int sum, int time, float rate);
 
 int is_even(int num)
 {
@@ -20,12 +21,12 @@ int is_odd(int num)
   return !is_even(num);
 }
 
-int square(int num)
+long int square(int num)
 {
   return num * num;
 }
 
-int cube(int num)
+long int cube(int num)
 {
   return square(num) * num;
 }
@@ -56,24 +57,37 @@ float average_of_three(int num1, int num2, int num3)
   return sum / 3;
 }
 
+float simple_interest(long int principle, int time, float rate)
+{
+  return (principle * time * rate) / 100;
+}
+
 int main(void)
 {
-  int num;
-  float temperature;
-  int number1, number2, number3;
+  int num, number1, number2, number3, time;
+  float temperature, rate;
+  long int principle;
+
   printf("Enter a number\n");
   scanf("%d", &num);
   printf("%d is %s\n", num, is_even(num) ? "even" : "not even");
   printf("%d is %s\n", num, is_odd(num) ? "odd" : "not odd");
-  printf("square of %d is %d\n", num, square(num));
-  printf("cube of %d is %d\n", num, cube(num));
+  printf("square of %d is %ld\n", num, square(num));
+  printf("cube of %d is %ld\n", num, cube(num));
+
   printf("Enter the temperature\n");
   scanf("%f", &temperature);
   printf("%f fahrenheit converted to centigrade is %f\n", temperature, convert_fahrenheit_to_centigrade(temperature));
   printf("%f centigrade converted to fahrenheit is %f\n", temperature, convert_centigrade_to_fahrenheit(temperature));
+
   printf("Enter three numbers for finding the greatest and also the average\n");
   scanf("%d %d %d", &number1, &number2, &number3);
   printf("The gretest among %d, %d, %d is %d\n", number1, number2, number3, greatest_of_three(number1, number2, number3));
   printf("The average of %d, %d, %d is %f\n", number1, number2, number3, average_of_three(number1, number2, number3));
+
+  printf("Enter principle, time and rate of interest to calculate simple interest\n");
+  scanf("%ld %d %f", &principle, &time, &rate);
+  printf("simple interest of principle %ld with rate of interest %f within time %d is %f\n", principle, rate, time, simple_interest(principle, time, rate));
+
   return 0;
 }
