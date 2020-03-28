0b4425d315b1afdb5b99e995a5b2a8c2d517e5eb
Sravani Labala <58027100+sravani-labala@users.noreply.github.com>
2020-03-28 22:22:11 +0530

    Added odd number series function

--- loops.c
+++ loops.c
@@ -2,6 +2,7 @@
 
 int factorial(int num);
 int fibonacci(int upto);
+int odd_number_series(int upto);
 
 // calculating upto 12 numbers
 int factorial(int num)
@@ -23,15 +24,29 @@ int fibonacci(int upto)
   return 0;
 }
 
+int odd_number_series(int upto)
+{
+  int num = 1;
+  while (upto)
+  {
+    (num % 2 != 0) && printf("%d ", num);
+    num++;
+    upto--;
+  }
+  return 0;
+}
+
 int main(void)
 {
-  int num;
+  int num1;
 
   printf("Enter a number\n");
-  scanf("%d", &num);
-  printf("factorial of %d is %d\n", num, factorial(num));
-  printf("The fibonacci series upto %d are as follows\n", num);
-  fibonacci(num);
+  scanf("%d", &num1);
+  printf("factorial of %d is %d\n", num1, factorial(num1));
+  printf("The fibonacci series upto %d are as follows\n", num1);
+  fibonacci(num1);
+  printf("\nThe odd number series upto %d is as follows\n", num1);
+  odd_number_series(num1);
 
   return 0;
 }
